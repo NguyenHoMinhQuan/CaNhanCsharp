e03c69f0735705289ee777b2b31bdee21bdaea1b
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-05 08:08:31 +0700

    Bài tập OOP tại lớp

---
+++ BaiTap2/BaiTap2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace BaiTap2
+{
+    public enum PointColor
+    {
+        LightBlue, BloodRed, Gold
+    }
+    public class Point     {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public PointColor Color { get; set; }
+        //constructor
+        public Point(double x, double y, PointColor color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Point Position: ({X}, {Y}), Point Color: {Color}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
