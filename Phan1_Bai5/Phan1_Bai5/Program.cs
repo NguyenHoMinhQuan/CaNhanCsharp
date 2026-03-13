8eb14f4d8d80e5c7b5e4682dfb95053711cab21c
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-13 19:26:03 +0700

    Bài 5

---
+++ Phan1_Bai5/Phan1_Bai5/Program.cs
@@ -0,0 +1,48 @@
+﻿// Nguyễn Hồ Minh Quân - 134
+// Tạo List chứa tên sinh viên,xóa tên do người dùng nhập vào
+using System;
+using System.Collections.Generic;
+
+namespace Phan1_Bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> students = new List<string>();
+            students.Add("Nguyen Van A");
+            students.Add("Le Thi B");
+            students.Add("Tran Van C");
+            students.Add("Pham Thi D");
+            students.Add("Hoang Van E");
+
+            Console.WriteLine("Danh sach sinh vien:");
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("\nNhap ten sinh vien can xoa:");
+            string nameToRemove = Console.ReadLine();
+
+            if (students.Remove(nameToRemove))
+            {
+                Console.WriteLine("Da xoa sinh vien: {0}", nameToRemove);
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sinh vien: {0}", nameToRemove);
+            }
+
+            Console.WriteLine("\nDanh sach sinh vien sau khi xoa:");
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("\nNhan phim bat ky de thoat...");
+            Console.ReadKey();
+        }
+    }
+}
