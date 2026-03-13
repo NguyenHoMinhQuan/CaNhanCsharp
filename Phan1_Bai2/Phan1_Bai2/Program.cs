3e394b8bb1c7ea59fe43a55b5c5e1a49a86561b1
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-13 19:24:15 +0700

    Bài 2

---
+++ Phan1_Bai2/Phan1_Bai2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Nhập các số nguyên vào List<int> Và tính tổng của chúng.
+    ﻿using System;
+    using System.Collections.Generic;
+namespace Phan1_Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Nhap cac so nguyen (nhap 'done' de ket thuc):");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "done")
+                {
+                    break;
+                }
+                if (int.TryParse(input, out int number))
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine("Vui long nhap 1 so nguyen hop le hoac 'done' de ket thuc.");
+                }
+            }
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            Console.WriteLine($"Tong cua cac so da nhap la: {sum}");
+        }
+    }
+}
