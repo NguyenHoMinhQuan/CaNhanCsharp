5bcf689eeaab226992b336291e75988396270c62
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-13 19:23:08 +0700

    Phần 1 _ bài tập 1

---
+++ Phan1_Bai1/Phan1_Bai1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Tạo danh sách số nguyên, thêm 5 số bất kì và in ra danh sách đó.
+    ﻿using System;
+    using System.Collections.Generic;
+namespace Phan1_Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten - MSSV: Minh Quân_23115053122134");
+            Console.WriteLine("------------------------------------------");
+            List<int> numbers = new List<int>();
+
+            numbers.Add(10);
+            numbers.Add(20);
+            numbers.Add(30);
+            numbers.Add(40);
+            numbers.Add(50);
+
+            Console.WriteLine("Danh sach so nguyen:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
