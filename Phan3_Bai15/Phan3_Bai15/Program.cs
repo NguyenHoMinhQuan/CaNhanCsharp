11920df69abf26da7212135085afeb821b363307
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-15 23:43:35 +0700

    Bài 15

---
+++ Phan3_Bai15/Phan3_Bai15/Program.cs
@@ -0,0 +1,47 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+using System;
+using System.Collections.Generic;
+
+namespace Phan2_Bai14_Updated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> tuDien = new Dictionary<string, string>();
+
+            tuDien.Add("hello", "xin chao");
+            tuDien.Add("world", "the gioi");
+            tuDien.Add("apple", "qua tao");
+            tuDien.Add("computer", "may tinh");
+            tuDien.Add("student", "sinh vien");
+
+            while (true)
+            {
+                Console.WriteLine("\nNhap tu tieng Anh (hoac nhap '0' de dung):");
+                string tuKhoa = Console.ReadLine();
+
+                if (tuKhoa == "0")
+                {
+                    Console.WriteLine("Dang thoat...");
+                    break;
+                }
+
+                tuKhoa = tuKhoa.ToLower();
+
+                if (tuDien.ContainsKey(tuKhoa))
+                {
+                    Console.WriteLine("=> Nghia cua '{0}' la: {1}", tuKhoa, tuDien[tuKhoa]);
+                }
+                else
+                {
+                    Console.WriteLine("=> Tu '{0}' chua co trong tu dien!", tuKhoa);
+                }
+
+                Console.WriteLine("------------------------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
