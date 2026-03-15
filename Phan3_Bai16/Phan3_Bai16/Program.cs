64ec883f1b4516ce58af57f6b3b89d74cf78bbd5
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-15 23:44:11 +0700

    Bài 16

---
+++ Phan3_Bai16/Phan3_Bai16/Program.cs
@@ -0,0 +1,45 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+using System;
+using System.Collections.Generic;
+
+namespace Phan2_Bai16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nNhap mot chuoi bat ky (hoac nhap '0' de dung):");
+                string input = Console.ReadLine();
+
+                if (input == "0") break;
+
+                Dictionary<char, int> countDict = new Dictionary<char, int>();
+
+                foreach (char c in input)
+                {
+                    if (countDict.ContainsKey(c))
+                    {
+                        countDict[c]++;
+                    }
+                    else
+                    {
+                        countDict[c] = 1;
+                    }
+                }
+
+                Console.WriteLine("Ket qua dem ky tu:");
+                foreach (KeyValuePair<char, int> entry in countDict)
+                {
+                    Console.WriteLine("{0} : {1}", entry.Key, entry.Value);
+                }
+
+                Console.WriteLine("------------------------------------------");
+            }
+
+            Console.WriteLine("Chuong trinh ket thuc.");
+            Console.ReadKey();
+        }
+    }
+}
