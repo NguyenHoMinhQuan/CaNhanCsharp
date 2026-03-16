9389a807515ee28cea58d2e72a64e7c096af1efd
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-16 19:58:58 +0700

    Bài 5

---
+++ linq_phan1_bai5/linq_phan1_bai5/Program.cs
@@ -0,0 +1,21 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace linq_phan1_bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            var squares = numbers.Select(x => x * x).ToList();
+            Console.WriteLine("Binh phuong cua moi so trong danh sach:");
+            foreach (var square in squares)
+            {
+                Console.WriteLine(square);
+            }
+        }
+    }
+}
