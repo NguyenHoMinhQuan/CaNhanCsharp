21742e74a0d04b08901b5cf6012379ef0d6de685
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-16 19:58:03 +0700

    Bài 3

---
+++ linq_phan1_bai3/linq_phan1_bai3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+// linq sắp xếp tăng dần
+    ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace linq_phan1_bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 2, 9, 1, 5, 6 };
+            var sortedNumbers = from num in numbers
+                                orderby num ascending
+                                select num;
+            Console.WriteLine("Sap xep tang dan:");
+            foreach (var num in sortedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
