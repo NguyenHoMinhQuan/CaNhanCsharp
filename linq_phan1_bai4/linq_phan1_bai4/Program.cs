15a02c9215c42f16802504fbc994433a7408092a
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-16 19:58:30 +0700

    Bài 4

---
+++ linq_phan1_bai4/linq_phan1_bai4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+//  sắp xếp giảm dần
+    ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace linq_phan1_bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 2, 9, 1, 5, 6 };
+            var sortedNumbers = from num in numbers
+                                orderby num descending
+                                select num;
+            Console.WriteLine("Sap xep giam dan:");
+            foreach (var num in sortedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
