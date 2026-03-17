592e66764a06e1b932585becffcdf1084f4fa9d3
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-17 20:09:40 +0700

    Bài 13

---
+++ linq_phan2_bai13/linq_phan2_bai13/Program.cs
@@ -0,0 +1,24 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+// sinh viên đầu cso điểm > 7
+// fisrtordefault
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace linq_phan2_bai13
+{
+    class Student { public int Id; public string Name; public double Score; }
+    class Program
+    {
+        static void Main()
+        {
+            List<Student> students = GetList();
+            var sv = students.FirstOrDefault(s => s.Score > 7);
+            if (sv != null) Console.WriteLine("Sv dau tien > 7: " + sv.Name);
+            Console.ReadKey();
+        }
+        static List<Student> GetList() => new List<Student> {
+            new Student{Id=1, Name="An", Score=8}, new Student{Id=2, Name="Binh", Score=6},
+            new Student{Id=3, Name="Chi", Score=9}, new Student{Id=4, Name="Dung", Score=7}
+        };
+    }
+}
