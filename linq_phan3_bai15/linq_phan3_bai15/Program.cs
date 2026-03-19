dff10ad0ca9927d25a6c3ff8e1f00718096e1aeb
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-19 21:14:00 +0700

    Bài 15

---
+++ linq_phan3_bai15/linq_phan3_bai15/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+//loại bỏ số trùng
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_phan2_bai15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+            Console.WriteLine("Danh sach ban dau: " + string.Join(" ", numbers));
+
+            var distinctNumbers = numbers.Distinct();
+
+            Console.WriteLine("Danh sach sau khi loai bo trung: " + string.Join(" ", distinctNumbers));
+
+            Console.WriteLine("------------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
