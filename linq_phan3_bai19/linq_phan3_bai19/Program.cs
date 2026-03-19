fffd1499bb89d85a38bf8713939b2f697c327e6b
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-19 21:15:52 +0700

    Bài 19

---
+++ linq_phan3_bai19/linq_phan3_bai19/Program.cs
@@ -0,0 +1,29 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_phan2_bai19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 3, 2, 3, 4, 3, 2, 5, 2, 2 };
+
+            Console.WriteLine("Danh sach: " + string.Join(" ", numbers));
+
+            var mostFrequent = numbers.GroupBy(n => n)
+                                      .OrderByDescending(g => g.Count())
+                                      .FirstOrDefault();
+
+            if (mostFrequent != null)
+            {
+                Console.WriteLine("\nGia tri xuat hien nhieu nhat la: {0}", mostFrequent.Key);
+                Console.WriteLine("So lan xuat hien: {0}", mostFrequent.Count());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
