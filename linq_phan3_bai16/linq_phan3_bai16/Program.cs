095ec2b9657ba1fffc6a89a5eaad366f627322c8
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-03-19 21:14:35 +0700

    Bài 16

---
+++ linq_phan3_bai16/linq_phan3_bai16/Program.cs
@@ -0,0 +1,18 @@
+﻿//Nguyễn Hồ Minh Quân_23115053122134_225LTC#02
+//dùng linq tính tổng, sum()
+    ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace linq_phan3_bai16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            int sum = numbers.Sum();
+            Console.WriteLine("Tong cua cac so la: " + sum);
+        }
+    }
+}
+
