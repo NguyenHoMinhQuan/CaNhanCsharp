119068b3331c0c0b7f8afbaebb5f190442eaab15
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2026-04-20 13:51:22 +0700

    Step 5: Cấu hình kết nối DB

--- BaiTapMVC/BaiTapMVC/obj/Debug/net8.0/BaiTapMVC.AssemblyInfo.cs
+++ BaiTapMVC/BaiTapMVC/obj/Debug/net8.0/BaiTapMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c510079febe4e2e1a1bf80ce8e96a528bde49ca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04042458ffb423ba6c3e90ed1405630d747946f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
